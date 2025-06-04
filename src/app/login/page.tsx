468974a18ddb78c3d0a8fbbf356bd34089aaa4a0
Vivek Majji <kavithamajji106@gmail.com>
2025-06-04 18:13:05 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <PiggyBank className="h-12 w-12 text-primary">
    
    Relevant files:
    
        - https://6000-firebase-studio-1749051953037.cluster-axf5tvtfjjfekvhwxwkkkzsk2y.cloudworkstations.dev/_next/static/chunks/node_modules_f31771ce._.js
        - https://6000-firebase-studio-1749051953037.cluster-axf5tvtfjjfekvhwxwkkkzsk2y.cloudworkstations.dev/_next/static/chunks/node_modules_f31771ce._.js
        - https://6000-firebase-studio-1749051953037.cluster-axf5tvtfjjfekvhwxwkkkzsk2y.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
        - https://6000-firebase-studio-1749051953037.cluster-axf5tvtfjjfekvhwxwkkkzsk2y.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    make the logo here some what up

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -60,7 +60,7 @@ export default function LoginPage() {
           <Card className="shadow-2xl">
             <CardHeader className="text-center">
               <div className="mb-4 flex items-center justify-center space-x-2">
-                <AppLogo className="h-12 w-12 text-primary" />
+                <AppLogo className="h-12 w-12 text-primary transform -translate-y-1" />
                 <h1 className="font-headline text-4xl font-bold text-primary">FinSight</h1>
               </div>
               <CardTitle className="font-headline text-2xl mb-2">Welcome Back</CardTitle>
